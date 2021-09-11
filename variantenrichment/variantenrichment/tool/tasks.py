e092293fa49af63d85daf7d053f4a6551c586553
olymie <ol.misevich@gmail.com>
2021-09-11 11:54:59 +0200

    test

--- variantenrichment/variantenrichment/tool/tasks.py
+++ variantenrichment/variantenrichment/tool/tasks.py
@@ -1,7 +1,8 @@
 from config.celery_app import app
 from .processes import assemble_case_sample, filter_samples_initial, filter_samples_final, \
     check_quality, count_statistics, check_cadd, cadd_filter_samples
-from .models import BackgroundJob
+from .models import BackgroundJob, ProjectFiles
+
 
 @app.task
 def annotate_task(bj_id):
@@ -41,14 +42,6 @@ def prefilter_task(bj_id):
     )
     bj_new.save()
     check_quality_task.apply_async(args=[bj_new.pk], countdown=1)
-
-    bj_new = BackgroundJob(
-        name="Filtering",
-        project=bj.project,
-        state="new"
-    )
-    bj_new.save()
-    filter_task.apply_async(args=[bj_new.pk], countdown=2)
 
 
 @app.task
@@ -92,6 +85,14 @@ def check_quality_task(bj_id):
     bj.state = "done"
     bj.save()
 
+    bj_new = BackgroundJob(
+        name="Filtering",
+        project=bj.project,
+        state="new"
+    )
+    bj_new.save()
+    filter_task.apply_async(args=[bj_new.pk], countdown=1)
+
 
 @app.task
 def stats_task(bj_id):
@@ -110,6 +111,8 @@ def check_cadd_task(bj_id):
     bj = BackgroundJob.objects.get(pk=bj_id)
     bj.state = "running"
     bj.save()
+
+    project_files = ProjectFiles.objects.get(project=bj.project)
 
     cadd_ready = check_cadd(project=bj.project)
 
